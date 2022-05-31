cabd6f6a459db6251405780b09070758182af535
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-05-31 14:33:18 -0400

    initial commit

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,56 @@
+import java.awt.Graphics;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class GamePanel {
+public class GamePanel extends Panel implements ActionListener{
+
+	GamePanel(){
+		
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollicsions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
 
 }
